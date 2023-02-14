51e55e507bf93c7550e82fc328c9785b80b4597c
uzik2 <Uzikanaut@gmail.com>
2023-02-14 18:28:58 +0200

    homework3.py

---
+++ homework3.py
@@ -0,0 +1,15 @@
+print("Дз 7")
+text = "Дуже люблю апельсинки"
+print(len(text.split(" ")))
+
+print("Дз 9")
+text = "123456789"
+print("a", text[2])
+print("b", text[-2])
+print("c", text[0:5])
+print("d", text[:-2])
+print("e", text[::2])
+print("f", text[1::2])
+print("g", text[::-1])
+print("h", text[::-2])
+print("i", len(text))
