166f0275f639033241da5cf08f75d5113cce5e60
uzik2 <Uzikanaut@gmail.com>
2023-02-21 21:37:03 +0200

    homework4.py

---
+++ homework4.py
@@ -0,0 +1,23 @@
+print("Дз 10")
+a = []
+counter = 0
+while True:
+    num = int(input("Введіть число"))
+    if num == 0:
+        break
+    a.append(num)
+for i in range(1, len(a)-1):
+    if a[i] > a[i - 1] and a[i] > a[i + 1]:
+        counter += 1
+print(counter)
+
+
+print("Дз 11")
+b = []
+while True:
+    num = int(input("Введіть число"))
+    if num == 0:
+        break
+    b.append(num)
+k = int(input("Введіть індекс"))
+b.pop(k), b.pop(), print(b)
