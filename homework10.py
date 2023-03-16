f61fc327f7825c031b8d0117153f68ccbea055dd
uzik2 <Uzikanaut@gmail.com>
2023-03-16 13:37:05 +0200

    homework10.py

---
+++ homework10.py
@@ -0,0 +1,9 @@
+print("Дз 22")
+with open('file.txt', 'a') as file:
+    file.seek(0)
+    while True:
+        line = input("Введіть рядок, або натисніть Enter, щоб закінчити введення: ")
+        if not line:
+            break
+        file.write(line + '\n')
+        print(line)
